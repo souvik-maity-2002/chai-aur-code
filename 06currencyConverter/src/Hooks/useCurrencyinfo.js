eff77bc16af4bc6eac0d89512e0a03708b053e7b
souvik-maity-2002 <iamsouvikmaity@2002gmail.com>
2026-01-07 12:14:13 +0530

    react router

---
+++ 06currencyConverter/src/Hooks/useCurrencyinfo.js
@@ -0,0 +1,16 @@
+import { useState,useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+    useEffect(()=>{
+        const [data,setData]=useState({})
+    fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json')
+    .then((res)=>res.json())
+    .then((res)=>setData(res[currency]))
+    console.log(data);
+    },[currency])
+    
+    return data
+}
+
+
+export default useCurrencyInfo;
